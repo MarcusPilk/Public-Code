2cf6150532f651aee0b2a4d831d91edc290d0358
Marcus Pilkington <mtpilk@essex.ac.uk>
2018-01-31 21:58:52 +0000

    Week4 WeeklyChallenges completed. "Spiral Ascension"

--- src/src/WeeklyChallenges/Week4/Spiral.java
+++ src/src/WeeklyChallenges/Week4/Spiral.java
@@ -6,8 +6,8 @@ import java.util.Scanner;
 public class Spiral {
     Scanner scanner;
     public static Integer[][] spiral;
-    ArrayList numbers;
-    static int num = 1;
+    static int num = 0;
+    static int loop;
     public static int x,y;
     static int spiralLength;
 
@@ -17,6 +17,7 @@ public class Spiral {
         System.out.println("Please choose length of spiral: ");
         spiralLength = Integer.parseInt(scanner.next());
         spiral = new Integer[spiralLength][spiralLength];
+        loop = spiralLength +0;
         x = 0;
         y = 0;
         makeSpiral();
@@ -28,92 +29,61 @@ public class Spiral {
     private void printSpiral() {
         for (int i = 0; i < spiralLength; i++) {
             for (int j = 0; j < spiralLength; j++) {
-                System.out.print(spiral[i][j]);
+                System.out.print(spiral[i][j]+"\t" );
             }
             System.out.println();
         }
     }
 
     private void makeSpiral() {
-        numbers = new ArrayList();
-        //while (num<(spiralLength*spiralLength-1)) {
-            RIGHT();
-            DOWN();
-            LEFT();
-            UP();
+        for (int i = 0; i < loop; i++) {
+            num++;
+            spiral[x][y] = num;
+            if(y+1<loop)y++;
+        }
 
-        //}
+        loop--;
+        while (num< spiralLength*spiralLength) {
+            DOWN(loop);
+            LEFT(loop);
+            loop--;
+            UP(loop);
+            RIGHT(loop);
+            loop--;
+        }
     }
 
-    private void RIGHT() {
-        System.out.println(x + " " + y);
-        while (y < spiralLength) {
-            if (spiral[x][y] != null) {
-                y++;
-            } else if (spiral[x][y] == null) {
-                spiral[x][y] = num;
-                numbers.add(num);
-                num++;
-                y++;
-            } else {
-                break;
-            }
+
+    private void DOWN(int loops) {
+
+        for (int i = 0; i < loops; i++) {
+            num++;
+            x++;
+            spiral[x][y] = num;
         }
-    y--;
     }
 
-    private void DOWN() {
-        System.out.println(x + " " + y);
-
-        while(x < spiralLength){
-            if(spiral[x][y] == null){
-                spiral[x][y] = num;
-                numbers.add(num);
-                num++;
-                x++;
-            }else if(spiral[x][y] != null){
-                x++;
-            }else{
-                break;
-            }
+    private void LEFT(int loops) {
+        for (int i = 0; i < loops; i++) {
+            num++;
+            y--;
+            spiral[x][y] = num;
         }
-        x--;
     }
 
-    private void LEFT() {
-        System.out.println(x + " " + y);
-
-        while (y >=0 ) {
-            if (spiral[x][y] != null) {
-                y--;
-            } else if (spiral[x][y] == null) {
-                spiral[x][y] = num;
-                numbers.add(num);
-                num++;
-                y--;
-            } else {
-                break;
-            }
+    private void UP(int loops) {
+        for (int i = 0; i < loops; i++) {
+            num++;
+            x--;
+            spiral[x][y] = num;
         }
-        y--;
     }
-
-    private void UP() {
-        System.out.println(x + " UP " + y);
-
-        while(x >=0){
-            if(spiral[x][y] == null){
-                spiral[x][y] = num;
-                numbers.add(num);
-                num++;
-                x--;
-            }else if(spiral[x][y] != null){
-                x--;
-            }else{
-                break;
-            }
+    private void RIGHT(int loops) {
+        for (int i = 0; i < loops; i++) {
+            num++;
+            y++;
+            spiral[x][y] = num;
         }
-        x++;
     }
 
 
